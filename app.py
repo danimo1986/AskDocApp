57c9dc1e3ec03f25f9ba642b14e1b794d3440709
TOSHIYA TANIMOTO <tanimoto.toshiya@jgc.com>
2023-09-14 20:46:18 +0900

    5th commmit for PDF

--- app.py
+++ app.py
@@ -1,28 +1,30 @@
 import streamlit as st
-from PyPDF2 import PdfReader  # 注意: PdfFileReaderは非推奨
+from PyPDF2 import PdfReader
 from langchain.llms import OpenAI
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.chains import RetrievalQA
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 def generate_response(uploaded_file, openai_api_key, query_text):
     # Load document if file is uploaded
     if uploaded_file is not None:
-        documents = [uploaded_file.read().decode()]
-        # Split documents into chunks
-        text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-        texts = text_splitter.create_documents(documents)
-        # Select embeddings
-        embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
-        # Create a vectorstore from documents
-        db = Chroma.from_documents(texts, embeddings)
-        # Create retriever interface
-        retriever = db.as_retriever()
-        # Create QA chain
-        qa = RetrievalQA.from_chain_type(llm=OpenAI(openai_api_key=openai_api_key), chain_type='stuff', retriever=retriever)
-        return qa.run(query_text)
+        pdf_text = get_pdf_text(uploaded_file)
+        if pdf_text:
+            # Split documents into chunks
+            text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+            texts = text_splitter.create_documents([pdf_text])
+            # Select embeddings
+            embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
+            # Create a vectorstore from documents
+            db = Chroma.from_documents(texts, embeddings)
+            # Create retriever interface
+            retriever = db.as_retriever()
+            # Create QA chain
+            qa = RetrievalQA.from_chain_type(llm=OpenAI(openai_api_key=openai_api_key), chain_type='stuff', retriever=retriever)
+            return qa.run(query_text)
+        else:
+            return "Error: PDF text extraction failed."
 
 # Page title
 st.set_page_config(page_title='🦜🔗 Ask the PDF App')
